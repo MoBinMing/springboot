348d6251ccf2c870d9fb16e10e0162d3cf2e4181
MoBinMing <1915809094@qq.com>
2020-03-08 21:59:36 +0800

    Ing

--- src/test/java/com/mobinming/springboot/UserTest.java
+++ src/test/java/com/mobinming/springboot/UserTest.java
@@ -1,33 +1,22 @@
 package com.mobinming.springboot;
 
-import com.mobinming.springboot.dao.UsersDao;
-import com.mobinming.springboot.entity.Users;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.Date;
 import java.util.List;
 
 @SpringBootTest
 public class UserTest {
-    @Autowired
-    private UsersDao usersDao;
+
 
     @Test
     void testMybatis() {
-        List<Users> users = usersDao.selectList(null);
-        for (Users u : users) {
-            System.out.println(u.toString());
-        }
-        Users user1= usersDao.selectByMyIdy(1);
-        System.out.println(user1.getName());
-
-        Users user=new Users();
-        user.setDate(new Date());
-        user.setId(9);
-        user.setName("666");
-        System.out.println("插入成功："+user.insert());
-
+//        Users users=new Users();
+//        users.setId(88);
+//        users.insert();
     }
 }
